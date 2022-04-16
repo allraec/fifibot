638172f82741122625ea60006213f84adb4dd16c
Allison Castaneda <allraec.dev@gmail.com>
2022-04-15 21:18:11 -0500

    test adding ephemeral channel

--- src/Bot.ts
+++ src/Bot.ts
@@ -3,9 +3,16 @@ import { IntentOptions } from "./config/IntentOptions";
 import ready from "./listeners/ready";
 import interactionCreate from "./listeners/interactionCreate";
 import * as dotenv from 'dotenv';
+import TempChannels from "discord-temp-channels";
 dotenv.config();
 
 const client = new Client({intents: IntentOptions});
+const tempChannels = new TempChannels(client);
 ready(client);
 interactionCreate(client);
+tempChannels.registerChannel("964709389409873940", {
+    childAutoDeleteIfEmpty: true,
+    childAutoDeleteIfOwnerLeaves: true,
+    childFormat: (member, count) => `#${count} | ${member.user.username}'s lounge`
+})
 client.login(process.env.BOT_TOKEN);
