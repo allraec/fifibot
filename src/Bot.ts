31f9f611a659e9aed5d1a88846af8a16e99581a7
Allison Castaneda <allraec.dev@gmail.com>
2022-05-04 21:35:46 -0500

    Add emoji

--- src/Bot.ts
+++ src/Bot.ts
@@ -14,6 +14,6 @@ tempChannels.registerChannel(`${process.env.CHANNEL_ID}`, {
     childCategory: `${process.env.CATEGORY_ID}`,
     childAutoDeleteIfEmpty: true,
     childAutoDeleteIfOwnerLeaves: true,
-    childFormat: (member, count) => `⎝⧹${member.user.username}'s Tent⧸⎠`
+    childFormat: (member) => `⧸${member.user.username}'s Tent🎪⧹`
 })
 client.login(process.env.BOT_TOKEN);
